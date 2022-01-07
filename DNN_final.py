edb7cf9d95a08fe0ae0474631f7befa132562dfd
CuckooWang <50347905+CuckooWang@users.noreply.github.com>
2022-01-07 22:51:03 +0800

    Update DNN_final.py

--- DNN_final.py
+++ DNN_final.py
@@ -1,158 +1,93 @@
-import numpy as np
-from Tools import WeightAndMatrix
-from Tools import dnn
+from Tools import getMMScoreType,WeightAndMatrix
 from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import KFold,StratifiedKFold
+from keras import metrics
+from keras.models import Model
+import numpy as np
+from keras import backend as K
+from keras import optimizers
+from keras.models import Sequential,load_model
+from keras.layers import Dense, Dropout
+from keras.callbacks import EarlyStopping,ModelCheckpoint
 def final_dnn():
-        length = 30
-        nfold = 10
-        f1 = open("POS.txt", "r")
-        f2 = open("NEG.txt", "r")
+    length = 30
+    nfold = 10
+    f1 = open(r"POS.txt", "r")
+    f2 = open(r"NEG.txt", "r")
+    pos = set()
+    neg = set()
+    for line in f1.readlines():
+        sp = line.strip().split("\t")
+        pep = sp[0]
+        site = sp[1] + "\t" + sp[2]
+        pos.add(pep)
+    for line in f2.readlines():
+        sp = line.strip().split("\t")
+        pep = sp[0]
+        if pep not in pos:
+            neg.add(pep)
 
-        pos = set()
-        neg = set()
-        print("Reading positive dataset")
-        for line in f1.readlines():
-            sp = line.strip().split("\t")
-            pep = sp[0]
-            pos.add(pep)
-        print("Reading negative dataset")
-        for line in f2.readlines():
-            sp = line.strip().split("\t")
-            pep = sp[0]
-            if pep not in pos:
-                neg.add(pep)
+    fw = open(r"AUCs.txt", "a")
+    AAscores, l_aas, weight_coef, AAs = \
+        WeightAndMatrix(r"traningout_best.txt")
+    l_scores, l_type, peps = getMMScoreType(pos, neg, AAscores, weight_coef, l_aas, AAs, length)
+    raw_scores = []
+    for i in range(len(l_scores)):
+        total = 0.0
+        for j in range(len(l_scores[i])):
+            total += l_scores[i][j]
+        raw_scores.append(total)
+    X = np.array(l_scores)
+    Y = np.array(l_type)
+    PEP = np.array(peps)
+    parameter = [512, 0.2, 2, X.shape[1]]
+    auc_all,best_model = dnn(X,Y,nfold,parameter,PEP)
+    fw.write("Best AUC:" + "\t" + str(auc_all) + "\t" + str(best_model) + "\n")
+    fw.flush()
+    fw.close()
 
-        AAscores, l_aas, weight_coef, AAs = \
-            WeightAndMatrix("traningout_best.txt")
-        l_scores, l_type, peps = getMMScoreType(pos, neg, AAscores, weight_coef, l_aas, AAs, length)
-        raw_scores = []
-        for i in range(len(l_scores)):
-            total = 0.0
-            for j in range(len(l_scores[i])):
-                total += l_scores[i][j]
-            raw_scores.append(total)
-        X = np.array(l_scores)
-        Y = np.array(l_type)
-        PEP = np.array(peps)
-        parameter = [512, 0.2, 2, X.shape[1]]
-        auc_all,best_model = dnn(X,Y,nfold,parameter,PEP)
-        print(auc_all)
 
-def getWeightScoreType(pos, neg, matrix, AAs,length):
-    scores = []
-    for i in range(length*2+1):
-        pos_score = []
-        for j in range(len(AAs)):
-            aa1 = AAs[j]
-            score = 0.0
-            for oth in pos:
-                aa2 = oth[i:i + 1]
-                aas = aa1 + "_" + aa2
-                aas2 = aa2 + "_" + aa1
-                if aas in matrix:
-                    score += matrix[aas]
-                else:
-                    score += matrix[aas2]
-            pos_score.append(score)
-        scores.append(pos_score)
 
-    l_scores = []
-    l_type = []
-    l_peps = []
+def dnn(X,Y,nfold,parameter,PEP):
+    skf = StratifiedKFold(n_splits=nfold)
+    num = 0
+    best_auc = 0.0
+    best_model = 0
+    Y_last = []
+    Score_last = []
+    for train_index, test_index in skf.split(X, Y):
+        num += 1
+        print("dnn_" + str(num))
+        X_train, X_test = X[train_index], X[test_index]
+        Y_train, Y_test = Y[train_index], Y[test_index]
+        model = create_model(parameter)
+        model.fit(X_train, Y_train, epochs=1000, batch_size=1024,validation_data=(X_test,Y_test),verbose=1,
+                  callbacks=[EarlyStopping(monitor="val_auc", mode="max", min_delta=0, patience=30),
+                             ModelCheckpoint(str(num) +'.model', monitor="val_auc", mode="max", save_best_only=True)])
+        model = load_model(str(num) +".model")
+        predict_x = model.predict(X_test)[:, 0]
+        auc = roc_auc_score(Y_test, predict_x)
+        if auc > best_auc:
+            best_auc = auc
+            best_model = num
+        Y_last.extend(Y_test)
+        Score_last.extend(predict_x)
+        K.clear_session()
+    auc_all = roc_auc_score(np.array(Y_last), np.array(Score_last))
+    return auc_all,best_model
 
-    for pep in pos:
-        score = []
-        for i in range(len(pep)):
-            aa = pep[i:i + 1]
-            index = AAs.index(aa)
-            aascore = (scores[i][index] - matrix[aa + "_" + aa]) / (len(pos) - 1)
-            score.append(aascore)
-        l_scores.append(score)
-        l_type.append(1)
-        l_peps.append(pep)
+def create_model(parameter):
+    model = Sequential()
+    model.add(Dense(parameter[0], activation='linear', input_dim=parameter[3]))
+    model.add(Dropout(parameter[1]))
+    for i in range(parameter[2]):
+        fold = 2 ** (i+1)
+        model.add(Dense(parameter[0] / fold,activation='linear'))
+        model.add(Dropout(parameter[1]))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(optimizer=optimizers.Adam(lr=1e-3,decay=3e-5), loss='binary_crossentropy', metrics=[metrics.AUC(name="auc")])
+    model.summary()
 
-    for pep in neg:
-        score = []
-        for i in range(len(pep)):
-            aa = pep[i:i + 1]
-            index = AAs.index(aa)
-            aascore = scores[i][index] / len(pos)
-            score.append(aascore)
-        l_scores.append(score)
-        l_type.append(0)
-        l_peps.append(pep)
-
-    return l_scores, l_type,l_peps
-
-def getMMScoreType(pos, neg, matrix, weights, l_aas, AAs, length):
-    scorespos = []
-    scoresneg = []
-    for i in range(length * 2 + 1):
-        score_pos = []
-        score_neg = []
-        for j in range(len(AAs)):
-            aa1 = AAs[j]
-            score = []
-            for z in range(len(l_aas)):
-                score.append(0.0)
-            for oth in pos:
-                aa2 = oth[i:i + 1]
-                aas1 = aa1 + "_" + aa2
-                aas2 = aa2 + "_" + aa1
-                if aas1 in l_aas:
-                    index = l_aas.index(aas1)
-                    score[index] += matrix[aas1] * weights[i]
-                elif aas2 in l_aas:
-                    index = l_aas.index(aas2)
-                    score[index] += matrix[aas2] * weights[i]
-            scoreneg = np.array(score)
-            index2 = l_aas.index(aa1 + "_" + aa1)
-            score[index2] -= matrix[aa1 + "_" + aa1] * weights[i]
-            scorepos = np.array(score)
-            score_pos.append(scorepos)
-            score_neg.append(scoreneg)
-        scorespos.append(score_pos)
-        scoresneg.append(score_neg)
-
-    l_scores = []
-    l_type = []
-    l_peps = []
-
-    for pep in pos:
-        score = getArray(l_aas)
-        for i in range(len(pep)):
-            aa = pep[i:i + 1]
-            index = AAs.index(aa)
-            scoreary = scorespos[i][index]
-            score += scoreary
-
-        score = (score / (len(pos) - 1)).tolist()
-
-        l_scores.append(score)
-        l_type.append(1)
-        l_peps.append(pep)
-
-    for pep in neg:
-        score = getArray(l_aas)
-        for i in range(len(pep)):
-            aa = pep[i:i + 1]
-            index = AAs.index(aa)
-            scoreary = scoresneg[i][index]
-            score += scoreary
-
-        score = (score / len(pos)).tolist()
-
-        l_scores.append(score)
-        l_type.append(0)
-        l_peps.append(pep)
-    return l_scores, l_type, l_peps
-
-def getArray(l_aas):
-    score = []
-    for i in range(len(l_aas)):
-        score.append(0.0)
-    scoreary = np.array(score)
-
-    return scoreary
+    return model
 
 final_dnn()
